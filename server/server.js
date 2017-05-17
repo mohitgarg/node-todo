e2ef82e8e2457118550f35eb694630eb21d4eca7
Mohit Garg <gargmohit@live.in>
2017-05-17 20:06:13 +0530

    DELETE route added

--- server/server.js
+++ server/server.js
@@ -45,6 +45,21 @@ app.get('/todos/:id', (req, res) => {
     .catch(err => res.status(404).send(err))
 })
 
+app.delete('/todos/:id', (req, res) => {
+  const id = req.params.id
+  if (!ObjectID.isValid(id)) {
+    res.status(404).send()
+  }
+  Todo.findByIdAndRemove(id)
+    .then(todo => {
+      if (!todo) {
+        return res.status(404).send()
+      }
+      res.send(todo)
+    })
+    .catch(err => res.status(400).send(err))
+})
+
 app.listen(port, () => {
   console.log(`App is running on ${port}`)
 })
